aa4f7ed5e9ef233d72ddc2049ba84de41dc1e370
ralbareda <ramon.albereda@sky.uk>
2020-06-17 10:50:43 +0200

    git commit -a

--- src/main/java/com/demo/crm/model/ContactsController.java
+++ src/main/java/com/demo/crm/model/ContactsController.java
@@ -3,9 +3,8 @@ package com.demo.crm.model;
 import java.net.URISyntaxException;
 import java.util.Collection;
 
-import javax.validation.Valid;
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:3000")
-class ContactsController {    
+class ContactsController {
     private ContactRepository contactRepository;
 
     public ContactsController(ContactRepository contactRepository) {
@@ -30,7 +28,7 @@ class ContactsController {
     }
 
     @PostMapping("/contacts")
-    ResponseEntity<Contact> createContact(@Valid @RequestBody Contact contact) throws URISyntaxException {
+    ResponseEntity<Contact> createContact(@Validated @RequestBody Contact contact) throws URISyntaxException {
         Contact result = contactRepository.save(contact);
         return ResponseEntity.ok().body(result);
     }
