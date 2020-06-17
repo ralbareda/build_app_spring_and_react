1db4337e721ec1558bf4bc3ff96db96df455f733
ralbareda <ramon.albereda@sky.uk>
2020-06-17 08:39:48 +0200

    git commit -a

---
+++ src/main/java/com/demo/crm/model/ReactAndSpringDataRestApplication.java
@@ -0,0 +1,14 @@
+package com.demo.crm.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestApplication {
+    public static void main(String[] args) {
+        /**
+         * Run the Server...
+         */
+        SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+    }
+}
